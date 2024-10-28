2d42f126793a2ed8fe9302cd3a47b7e8c5b7c28a
Abhinav Prakash <115349543+Rishiyaduwanshi@users.noreply.github.com>
2024-10-29 01:25:25 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,18 +1,10 @@
 <?php
 session_start();
-if ($_SERVER['HTTP_HOST'] == 'localhost:8012') {
-    define("BASE_URL", "http://localhost:8012/lms/");
-    define("DIR_URL", $_SERVER['DOCUMENT_ROOT'] . "/lms/");
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    define("BASE_URL", "http://localhost/");
+    define("DIR_URL", $_SERVER['DOCUMENT_ROOT'] . "/");
     define("SERVER_NAME", "localhost");
     define("USERNAME", "root");
     define("PASSWORD", "");
     define("DATABASE", "lms");
-} else {
-    define("BASE_URL", "https://lms.com");
-    define("DIR_URL", $_SERVER['DOCUMENT_ROOT']);
-
-    define("SERVER_NAME", "");
-    define("USERNAME", "");
-    define("PASSWORD", "");
-    define("DATABASE", "");
-}
+} 
