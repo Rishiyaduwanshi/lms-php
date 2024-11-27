e210373ee662a467e42658d394bb46ece49b35a1
Abhinav Prakash <abprakash85409@gmail.com>
2024-11-28 01:37:53 +0530

    Add -> env

--- config/config.php
+++ config/config.php
@@ -1,10 +1,18 @@
 <?php
 session_start();
-if ($_SERVER['HTTP_HOST'] == 'localhost') {
-    define("BASE_URL", "http://localhost/");
+require_once 'vendor/autoload.php';
+
+// Load environment variables from the .env file
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+if ($_SERVER['HTTP_HOST'] == 'walrus-app-65ogh.ondigitalocean.app') {
+    define("BASE_URL", "https://walrus-app-65ogh.ondigitalocean.app/");
     define("DIR_URL", $_SERVER['DOCUMENT_ROOT'] . "/");
-    define("SERVER_NAME", "localhost");
-    define("USERNAME", "root");
-    define("PASSWORD", "");
-    define("DATABASE", "lms");
-} 
+    // Access environment variables and define constants
+    define("SERVER_NAME", getenv("DB_HOST"));
+    define("USERNAME", getenv("DB_USERNAME"));
+    define("PASSWORD", getenv("DB_PASSWORD"));
+    define("DATABASE", getenv("DB_DATABASE"));
+    define("PORT", getenv("DB_PORT"));
+}
